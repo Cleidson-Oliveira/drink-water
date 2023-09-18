93dd0ac27cf6965ef5399fdb9b58c0a6b99dfc3a
Cleidson Oliveira <cleidsonoliveira00@gmail.com>
2023-09-17 21:56:29 -0300

    chore: add inicial config

---
+++ src/app/(tabs)/hydratation-records.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+export default function HydratationRecords () {
+
+    return (
+        <View>
+            <Text>
+                Histórico
+            </Text>
+        </View>
+    )
+}
---
+++ src/components/waterCuriosityCard/style.ts
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        paddingVertical: 24,
+        paddingHorizontal: 16,
+        backgroundColor: "#3388cc",
+        borderRadius: 8,
+        flexDirection: "row",
+        gap: 8,
+        alignItems: "flex-start"
+    },
+    content: {
+        paddingLeft: 8,
+        paddingTop: 4,
+        maxWidth: "60%"
+    },
+    text: {
+        color: "#fff",
+        fontSize: 18,
+    }
+})
