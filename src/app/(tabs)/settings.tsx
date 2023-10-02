c070d9b71f56db065b601750e49f7ff6e80010d7
Cleidson Oliveira <cleidsonoliveira00@gmail.com>
2023-10-01 22:01:40 -0300

    chore: add cup size control to settings screen

--- src/app/(tabs)/settings.tsx
+++ src/app/(tabs)/settings.tsx
@@ -1,12 +1,37 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import { Button } from "../../components/button";
 
 export default function Settings () {
 
     return (
         <View>
-            <Text>
-                Configurações
-            </Text>
+            <View style={styles.configBox}>
+                <Text style={styles.configTitle}>Cup size (ml)</Text>
+                <View style={styles.buttonsContainer}>
+                    <Button textContent="100" />
+                    <Button textContent="200" />
+                    <Button textContent="300" />
+                    <Button textContent="400" />
+                    <Button textContent="500" />
+                </View>
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    configBox: {
+        padding: 8,
+        borderBottomColor: "#aaa",
+        borderBottomWidth: 1,
+        gap: 8,
+    }, 
+    configTitle: {
+        fontSize: 24,
+        fontWeight: "500"
+    },
+    buttonsContainer: {
+        gap: 16,
+        flexDirection: "row"
+    }
+})
